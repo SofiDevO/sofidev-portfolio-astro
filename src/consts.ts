9f2d0d019b82dd3758ad68bb70043391da9b1067
SofiDevO <angiemeow89@gmail.com>
2023-11-21 14:52:34 -0600

    :poop: Just fixing responsive,rename classes for CardTerminal.astro
    Some classes was renamed, more info in the README.md. Component TextTerminal was removed do to it was

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'SofiDev';
-export const SITE_DESCRIPTION = 'Frontend Developer';
