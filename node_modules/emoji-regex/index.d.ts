f95d1dbaa0a525c8376a821c90858b327223fda0
SofiDevO <angiemeow89@gmail.com>
2023-11-26 12:35:57 -0600

    fixing responsive

--- node_modules/emoji-regex/index.d.ts
+++ node_modules/emoji-regex/index.d.ts
@@ -1,3 +1,23 @@
 declare module 'emoji-regex' {
-  export default function emojiRegex(): RegExp;
+    function emojiRegex(): RegExp;
+
+    export default emojiRegex;
+}
+
+declare module 'emoji-regex/text' {
+    function emojiRegex(): RegExp;
+
+    export default emojiRegex;
+}
+
+declare module 'emoji-regex/es2015' {
+    function emojiRegex(): RegExp;
+
+    export default emojiRegex;
+}
+
+declare module 'emoji-regex/es2015/text' {
+    function emojiRegex(): RegExp;
+
+    export default emojiRegex;
 }
