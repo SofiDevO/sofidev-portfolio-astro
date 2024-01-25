ac80a9f22bee20dfaec133645e54ab3a2512d48e
SofiDevO <angiemeow89@gmail.com>
2024-01-25 15:31:08 -0800

    :sparkles: Changes in styles and navigation
    Adding new styles in all sections

--- src/data/ToolsInfo.js
+++ src/data/ToolsInfo.js
@@ -1,82 +1,78 @@
 const toolsInfo = [
   {
     title: "Slack",
-    link: "https://es.wikipedia.org/wiki/Bash",
     src: "./svg/slack.svg",
   },
   {
     title: "git",
-    link: "https://es.wikipedia.org/wiki/Git",
+
     src: "./svg/git.svg",
   },
   {
     title: "Bash",
-    link: "https://es.wikipedia.org/wiki/Bash",
+
     src: "./svg/terminal-fill.svg",
   },
   {
     title: "Notion",
-    link: "https://es.wikipedia.org/wiki/Notion",
+
     src: "./svg/notion-icon.svg",
   },
   {
     title: "Ubuntu",
-    link: "https://es.wikipedia.org/wiki/Ubuntu",
     src: "./svg/ubuntu.svg",
   },
   {
     title: "VsCode",
-    link: "https://visualstudio.microsoft.com/",
     src: "./svg/visual-studio-code.svg",
   },
   {
     title: "Figma",
-    link: "https://www.figma.com/",
+
     src: "./svg/figma.svg",
   },
   {
     title: "Woocommerce",
-    link: "https://es.wikipedia.org/wiki/WooCommerce",
+
     src: "./svg/woocommerce.svg",
   },
   {
     title: "Yoast SEO",
-    link: "https://en.wikipedia.org/wiki/Yoast_SEO",
+
     src: "./svg/yoast.svg",
   },
   {
     title: "XAMPP",
-    link: "https://www.apachefriends.org/es/index.html",
+
     src: "./svg/xampp.svg",
   },
   {
     title: "ELEMENTOR",
-    link: "https://elementor.com/",
+
     src: "./svg/elementor (1).svg",
   },
   {
     title: "Analytics",
-    link: "https://analytics.google.com/analytics/academy/course/6?hl=es",
+
     src: "./svg/google-analytics.svg",
   },
   {
     title: "Search Console",
-    link: "https://search.google.com/search-console",
+
     src: "./svg/google-search-console.svg",
   },
   {
     title: "Photoshop",
-    link: "https://es.wikipedia.org/wiki/Adobe_Photoshop",
+
     src: "./svg/photoshop.svg",
   },
   {
     title: "Illustrator",
-    link: "https://es.wikipedia.org/wiki/Adobe_Illustrator",
     src: "./svg/illustrator.svg",
   },
   {
     title: "Premier",
-    link: "https://es.wikipedia.org/wiki/Adobe_Premiere_Pro",
+
     src: "./svg/premiere.svg",
   },
 ];
