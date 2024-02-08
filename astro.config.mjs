c24277f1c220c8fa2bf0288a229d10f40110f554
SofiDevO <angiemeow89@gmail.com>
2024-02-08 06:04:22 -0800

    Se implementa filtro en Portafolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
-
 import vue from "@astrojs/vue";
+
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
   renderers: ['@astrojs/renderer-react'],
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), vue()]
+  integrations: [mdx(), sitemap(), react(), vue(), solidJs()]
 });
