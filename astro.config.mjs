5daaf516383eb3f9cf3e6d4abdb9603d2c9d1d19
SofiDev <angiemeow89@gmail.com>
2024-08-15 18:30:34 -0600

    ✨ Custom higthLith syntax for blog post code fragments

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,20 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://shiki.style/themes
+      theme: 'synthwave-84',
+      defaultColor: false,
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://shiki.style/languages
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
   renderers: ['@astrojs/renderer-react'],
   site: 'https://itssofi.dev/',
   integrations: [mdx(), sitemap(), react()]
